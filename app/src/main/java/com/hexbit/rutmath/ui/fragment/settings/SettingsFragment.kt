fe7c944ac5d1de3484dc0641405e5a2fd7c52dbe
= <=>
2024-08-12 09:59:04 +0700

    feat: Upgrade Gradle to compile to SDK 35

--- app/src/main/java/com/hexbit/rutmath/ui/fragment/settings/SettingsFragment.kt
+++ app/src/main/java/com/hexbit/rutmath/ui/fragment/settings/SettingsFragment.kt
@@ -66,6 +66,9 @@ class SettingsFragment() : BaseFragment() {
                 rbPortuguese.id -> {
                     setPreviewText("pt")
                 }
+                rbIndonesia.id -> {
+                    setPreviewText("in")
+                }
                 else -> error("error while checking")
             }
         }
@@ -136,6 +139,7 @@ class SettingsFragment() : BaseFragment() {
             "pl" -> rbPolish.isChecked = true
             "fr" -> rbFrench.isChecked = true
             "pt" -> rbPortuguese.isChecked = true
+            "in" -> rbIndonesia.isChecked = true
             else -> rbEnglish.isChecked = true
         }
     }
